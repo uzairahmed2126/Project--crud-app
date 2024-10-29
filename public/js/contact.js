147f410b9ed50be786fe3b3e0d2dff45855d3e03
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-10-30 01:06:27 +0530

    crud

---
+++ public/js/contact.js
@@ -0,0 +1,8 @@
+const body = document.querySelector("body");
+const div = createElement("div", "main-container");
+function createElement(elementName, idName) {
+  const createElement = document.createElement(elementName);
+  createElement.id = idName;
+  createElement.innerHTML = "<h1>I'm CONTACT Page</h1>";
+  body.append(createElement);
+}
